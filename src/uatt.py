6693e67082ccddc0cec6678ef97cf847d5e51271
Uri Goren <uri@goren4u.com>
2021-11-14 23:47:35 +0200

    Add files via upload

--- src/uatt.py
+++ src/uatt.py
@@ -1,12 +1,12 @@
 from urllib import request, parse
-import json
+import json, random
 
 def submit(name,company,email,phone,user, test_data):
     """Submits a test data set to the pydata server, and returns accuracy"""
     try:
         test_data = {str(k).lower().replace(".txt", ""):str(v).lower().strip() for k,v in test_data.items()}
         data = parse.urlencode({"user":user, "submission": json.dumps(test_data), "name":name,"company":company,"phone":phone,"email":email}).encode()
-        req =  request.Request("http://goren.ml/uattcontract/index.php", data=data)
+        req =  request.Request("https://goren.ml/uattcontract/index.php?rand="+str(random.randint(167,1000000)), data=data)
         resp = request.urlopen(req)
         return float(resp.read().decode("utf8"))
     except Exception as e:
