342a202c2cf9c1faf18491b99edfe35288e2485c
Uri Goren <uri@goren4u.com>
2021-11-14 23:17:48 +0200

    Update uatt.py

--- src/uatt.py
+++ src/uatt.py
@@ -6,7 +6,7 @@ def submit(name,company,email,phone,user, test_data):
     try:
         test_data = {str(k).lower().replace(".txt", ""):str(v).lower().strip() for k,v in test_data.items()}
         data = parse.urlencode({"user":user, "submission": json.dumps(test_data), "name":name,"company":company,"phone":phone,"email":email}).encode()
-        req =  request.Request("http://3.249.199.3/index.php", data=data)
+        req =  request.Request("http://goren.ml/uattcontract/index.php", data=data)
         resp = request.urlopen(req)
         return float(resp.read().decode("utf8"))
     except Exception as e:
